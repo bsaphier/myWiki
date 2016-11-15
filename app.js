10b77d5d977732c0520cd08545b0b965258c116d
Benjamin Saphier <b.saphier@gmail.com>
2016-11-14 23:00:54 -0500

    some basic structure

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ const bodyparser = require('body-parser');
 const nunjucks = require('nunjucks');
 const env = nunjucks.configure('views', {noCache: true});
 
-app.use(express.static('public'));
+const models = require('./models');
+const wikiRouter = require('./routes/wiki');
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 
 app.use(morgan('dev'));
+app.use(express.static('public'));
 
-app.listen(3001, () => {
-});
+app.use('/wiki', wikiRouter);
+
+models.User.sync()
+.then( () => {
+  return models.Page.sync();
+})
+.then( () => {
+  app.listen(3001, () => {
+    console.log('sSssSSSssssslowowlsssssSSSssSs');
+  });
+})
+.catch(console.error);
--- models/index.js
+++ models/index.js
@@ -1,22 +1,46 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+  logging: false
+});
 
 const Page = db.define('page', {
   title: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   urlTitle: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   content: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT,
+    allowNull: false
   },
   status: {
     type: Sequelize.ENUM('open', 'closed')
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+    }
+}, {
+  getterMethods: {
+    route: function() {
+      return '/wiki/' + this.urlTitle;
+    }
   }
 });
 
 const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    isEmail: true,
+    allowNull: false
+  }
 });
 
 module.exports = {
